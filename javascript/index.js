9b853f1d5bc021c13d5af196c210223dc7296651
sparksdaniellea <137011689+sparksdaniellea@users.noreply.github.com>
2023-08-20 13:06:14 -0400

    cleaned up js code

--- javascript/index.js
+++ javascript/index.js
@@ -1,39 +1,51 @@
 function updateTime() {
   let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = document.querySelector("#new-york-date");
-  let newYorkTimeElement = document.querySelector("#new-york-time");
-  newYorkDateElement.innerHTML = moment().format("dddd, MMMM DD, YYYY");
-  newYorkTimeElement.innerHTML = moment()
-    .tz("America/New_York")
-    .format("h:mm:ss A");
+  if (newYorkElement) {
+    let newYorkDateElement = document.querySelector("#new-york-date");
+    let newYorkTimeElement = document.querySelector("#new-york-time");
+    newYorkDateElement.innerHTML = moment().format("dddd, MMMM DD, YYYY");
+    newYorkTimeElement.innerHTML = moment()
+      .tz("America/New_York")
+      .format("h:mm:ss A");
+  }
 
   let johannesburgElement = document.querySelector("#johannesburg");
-  let johannesburgDateElement = document.querySelector("#johannesburg-date");
-  let johannesburgTimeElement = document.querySelector("#johannesburg-time");
-  johannesburgDateElement.innerHTML = moment().format("dddd, MMMM DD, YYYY");
-  johannesburgTimeElement.innerHTML = moment()
-    .tz("Africa/Johannesburg")
-    .format("h:mm:ss A");
+  if (johannesburgElement) {
+    let johannesburgDateElement = document.querySelector("#johannesburg-date");
+    let johannesburgTimeElement = document.querySelector("#johannesburg-time");
+    johannesburgDateElement.innerHTML = moment().format("dddd, MMMM DD, YYYY");
+    johannesburgTimeElement.innerHTML = moment()
+      .tz("Africa/Johannesburg")
+      .format("h:mm:ss A");
+  }
 
   let tokyoElement = document.querySelector("#tokyo");
-  let tokyoDateElement = document.querySelector("#tokyo-date");
-  let tokyoTimeElement = document.querySelector("#tokyo-time");
-  tokyoDateElement.innerHTML = moment().format("dddd, MMMM DD, YYYY");
-  tokyoTimeElement.innerHTML = moment().tz("Asia/Tokyo").format("h:mm:ss A");
+  if (tokyoElement) {
+    let tokyoDateElement = document.querySelector("#tokyo-date");
+    let tokyoTimeElement = document.querySelector("#tokyo-time");
+    tokyoDateElement.innerHTML = moment().format("dddd, MMMM DD, YYYY");
+    tokyoTimeElement.innerHTML = moment().tz("Asia/Tokyo").format("h:mm:ss A");
+  }
 
   let parisElement = document.querySelector("#paris");
-  let parisDateElement = document.querySelector("#paris-date");
-  let parisTimeElement = document.querySelector("#paris-time");
-  parisDateElement.innerHTML = moment().format("dddd, MMMM DD, YYYY");
-  parisTimeElement.innerHTML = moment().tz("Europe/Paris").format("h:mm:ss A");
+  if (parisElement) {
+    let parisDateElement = document.querySelector("#paris-date");
+    let parisTimeElement = document.querySelector("#paris-time");
+    parisDateElement.innerHTML = moment().format("dddd, MMMM DD, YYYY");
+    parisTimeElement.innerHTML = moment()
+      .tz("Europe/Paris")
+      .format("h:mm:ss A");
+  }
 
   let sydneyElement = document.querySelector("#sydney");
-  let sydneyDateElement = document.querySelector("#sydney-date");
-  let sydneyTimeElement = document.querySelector("#sydney-time");
-  sydneyDateElement.innerHTML = moment().format("dddd, MMMM DD, YYYY");
-  sydneyTimeElement.innerHTML = moment()
-    .tz("Australia/Sydney")
-    .format("h:mm:ss A");
+  if (sydneyElement) {
+    let sydneyDateElement = document.querySelector("#sydney-date");
+    let sydneyTimeElement = document.querySelector("#sydney-time");
+    sydneyDateElement.innerHTML = moment().format("dddd, MMMM DD, YYYY");
+    sydneyTimeElement.innerHTML = moment()
+      .tz("Australia/Sydney")
+      .format("h:mm:ss A");
+  }
 }
 
 setInterval(updateTime, 1000);
