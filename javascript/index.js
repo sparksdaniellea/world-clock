a2eab8fd59da4100fea9690213fb3b33d73911ca
sparksdaniellea <137011689+sparksdaniellea@users.noreply.github.com>
2023-08-20 15:15:59 -0400

    added ability to search for current location

--- javascript/index.js
+++ javascript/index.js
@@ -50,6 +50,9 @@ function updateTime() {
 
 function changeCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityDate = moment().tz(cityTimeZone).format("dddd, MMMM DD, YYYY");
   let cityTime = moment().tz(cityTimeZone).format("h:mm:ss A");
